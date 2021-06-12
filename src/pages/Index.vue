87a9d1d1f488611723e22a9a574d5d6aea3be146
dmosch <85744127+dmosch@users.noreply.github.com>
2021-06-12 16:28:07 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <Hero />
       <iframe src="https://www.google.com/maps/d/embed?mid=1QYGsiVQbqc1EShkHCZKout43QDP10q4F" width="980" height="720"></iframe>
-      <ProjectsGrid :projects="$page.projects.edges" />
+      <!-- <ProjectsGrid :projects="$page.projects.edges" /> -->
     </div>
     <LatestJournals :journals="$page.journals.edges" />
   </Layout>
