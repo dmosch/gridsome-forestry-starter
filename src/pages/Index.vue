feb0417d024454b59c2bed1a5cc5e7bb53f94d14
dmosch <85744127+dmosch@users.noreply.github.com>
2021-06-19 19:56:26 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -95,9 +95,16 @@ export default {
   width: 100%;
 }
 
-.container {height: 100%; width:100%; margin-top: 20px; margin-bottom: 20px; font-size: 0;}
-.legend, .legend, .legend, .legend, .legend, .legend, .legend_long {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
+.container_line1 {height: 100%; width:100%; margin-top: 20px; margin-bottom: 5px; font-size: 0;}
+.legend, .legend, .legend, .legend {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
 .legend {width: 25%;}
+.icon, .text {display: inline-block; *display: inline; zoom: 1; vertical-align: middle; font-size: 14px;}
+.icon {width: 10%;}
+.text {margin-left: 10px; margin-right: 15px;}
+
+.container_line2 {height: 100%; width:100%; margin-top: 5px; margin-bottom: 20px; font-size: 0;}
+.legend, .legend, .legend {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
+.legend {width: 33%;}
 .icon, .text {display: inline-block; *display: inline; zoom: 1; vertical-align: middle; font-size: 14px;}
 .icon {width: 10%;}
 .text {margin-left: 10px; margin-right: 15px;}
