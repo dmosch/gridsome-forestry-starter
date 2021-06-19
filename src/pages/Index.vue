07b49dae85525f2eb35dce9bd492c4b16ee4e8a6
dmosch <85744127+dmosch@users.noreply.github.com>
2021-06-19 20:07:13 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,33 +6,33 @@
       	<iframe src="https://www.google.com/maps/d/embed?mid=1QYGsiVQbqc1EShkHCZKout43QDP10q4F" width="980" height="720" class="map"></iframe>
       	<!-- <ProjectsGrid :projects="$page.projects.edges" /> -->
 	<div class="container_line1">
-	    <div class="legend">
+	    <div class="legend1">
 	    	<div class="icon"> <img src="../../uploads/maps-blue.svg" /> </div>
 	    	<div class="text"> Búnquers </div>
 	    </div>
-	    <div class="legend">
+	    <div class="legend1">
 	    	<div class="icon"> <img src="../../uploads/maps-yellow.svg" /> </div>
 	    	<div class="text"> Museos </div>
 	    </div>
-	    <div class="legend">
+	    <div class="legend1">
 	    	<div class="icon"> <img src="../../uploads/maps-uboot.svg" /> </div>
 	    	<div class="text"> U-Boot </div>
 	    </div>
-	    <div class="legend">
+	    <div class="legend1">
 	    	<div class="icon"> <img src="../../uploads/maps-blockhaus.svg" /> </div>
 	    	<div class="text"> Blockhaus </div>
 	    </div>
 	</div>
 	<div class="container_line2">
-	    <div class="legend">
+	    <div class="legend2">
 	    	<div class="icon"> <img src="../../uploads/maps-black.svg" /> </div>
 	    	<div class="text"> Cementerios </div>
 	    </div>
-	    <div class="legend">
+	    <div class="legend2">
 	    	<div class="icon"> <img src="../../uploads/maps-memorial.svg" /> </div>
 	    	<div class="text"> Memoriales </div>
 	    </div>
-	    <div class="legend">
+	    <div class="legend2">
 	    	<div class="icon"> <img src="../../uploads/maps-green.svg" /> </div>
 	    	<div class="text"> Campos de concentración </div>
 	    </div>
@@ -101,15 +101,15 @@ export default {
 .container_line1, .container_line2 {display: block; zoom: 1; text-align: center; font-size: 0;}
 
 .container_line1 {height: 100%; width:100%; margin-top: 20px; margin-bottom: 5px; font-size: 0;}
-.legend, .legend, .legend, .legend {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
-.legend {width: 25%;}
+.legend1, .legend1, .legend1, .legend1 {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
+.legend1 {width: 25%;}
 .icon, .text {display: inline-block; *display: inline; zoom: 1; vertical-align: middle; font-size: 14px;}
 .icon {width: 10%;}
 .text {margin-left: 10px; margin-right: 15px;}
 
 .container_line2 {height: 100%; width:100%; margin-top: 5px; margin-bottom: 20px; font-size: 0;}
-.legend, .legend, .legend {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
-.legend {width: 33%;}
+.legend2, .legend2, .legend2 {display: inline-block; *display: inline; zoom: 1; vertical-align: middle;  text-align: center; font-size: 0;}
+.legend2 {width: 33%;}
 .icon, .text {display: inline-block; *display: inline; zoom: 1; vertical-align: middle; font-size: 14px;}
 .icon {width: 10%;}
 .text {margin-left: 10px; margin-right: 15px;}
