7ab1f1593fee6f1265e03cd43a726ea6ab05a128
dmosch <85744127+dmosch@users.noreply.github.com>
2021-06-28 17:21:02 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "theBunkerMap",
+  siteUrl: `https://gridsomebunkermap.netlify.app`,
   host: "0.0.0.0",
   titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  siteDescription: "El lugar dónde encontrar todos los búnquers de la Segunda Guerra Mundial",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
